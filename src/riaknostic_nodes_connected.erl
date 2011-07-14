88de9dbd734c7f144b4ed48b3aec17508f33c3c8
Ian Plosker <iman1003@gmail.com>
2011-07-14 09:17:50 -0400

    Added more output to nodes connected.

--- src/riaknostic_nodes_connected.erl
+++ src/riaknostic_nodes_connected.erl
@@ -3,15 +3,23 @@
 
 handle_command(Config) ->
   Stats = dict:fetch(riak_stats, Config),
+  
   {connected_nodes, ConnectedNodes} = lists:keyfind(connected_nodes, 1, Stats),
   {ring_members, RingMembers} = lists:keyfind(ring_members, 1, Stats),
   {nodename, NodeName} = lists:keyfind(nodename, 1, Stats),
-  io:format("Checking connected nodes~n"),
-  lists:foreach(fun(Node) ->
+  io:format("Checking connected nodes: ~p~n", [ConnectedNodes]),
+  AllNodesInRingConnected = lists:all(fun(Node) ->
     case lists:member(Node, ConnectedNodes) of
-      true -> ok;
-      false when Node =:= NodeName -> ok;
-      false -> io:format("Node ~s is part of the ring but not connected to this node.", [Node])
+      true ->
+        true;
+      false when Node =:= NodeName ->
+        true;
+      false ->
+        io:format("Node ~s is part of the ring but not connected to this
+          node.", [Node]),
+          false
     end
   end, RingMembers),
+  
+  io:format("All nodes in ring connected: ~p~n",[AllNodesInRingConnected]),
   ok.
