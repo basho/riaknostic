9bb0c59793704182ebb115a3ad8bfcbb3056e8fe
Brian Sparrow <bsparrow@basho.com>
2012-10-04 10:43:51 -0300

    Removed freeBSD stuff

--- src/riaknostic_check_sysctl.erl
+++ src/riaknostic_check_sysctl.erl
@@ -43,19 +43,6 @@
                        {"net.ipv4.tcp_tw_reuse",                1, eq}
                       ]).
 
--define(FREEBSD_PARAMS, [
-                        %{"vm.swappiness",                        0, gte}, 
-                         {"net.core.wmem_default",          8388608, lte},
-                         {"net.core.rmem_default",          8388608, lte},
-                         {"net.core.wmem_max",              8388608, lte},
-                         {"net.core.rmem_max",              8388608, lte},
-                         {"net.core.netdev_max_backlog",      10000, lte},
-                         {"net.core.somaxconn",                4000, lte},
-                         {"net.ipv4.tcp_max_syn_backlog",     40000, lte},
-                         {"net.ipv4.tcp_fin_timeout",            15, gte},
-                         {"net.ipv4.tcp_tw_reuse",                1, eq}
-                        ]).
-
 -spec description() -> string().
 description() ->
     "Check sysctl tuning parameters".
@@ -69,7 +56,7 @@ check() ->
     Params = case os:type() of
                  {unix, linux} -> ?LINUX_PARAMS;
                  {unix, darwin} -> []; 
-                 {unix, freebsd} -> ?FREEBSD_PARAMS;
+                 {unix, freebsd} -> [];
                  {unix, sunos} -> []
              end,
     Recs = check_params(Params),
